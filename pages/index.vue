930fcbcac9557fe68f3a68e08b9221da426cc501
Alejandro Martos <alexmartos96@gmail.com>
2022-07-02 01:55:17 -0700

    much better

--- pages/index.vue
+++ pages/index.vue
@@ -9,25 +9,22 @@
       overflow: auto;
     "
   >
-    <div>
-      <!-- Image and text -->
-      <b-navbar variant="white" type="light">
-        <b-navbar-brand href="#">
-          <img
-            src="/images/logo.svg"
-            class="d-inline-block align-top"
-            alt="Kitten"
-          />
-        </b-navbar-brand>
-        <b-navbar-nav>
-          <b-nav-item active style="font-weight: 600" href="#">Tech</b-nav-item>
-          <b-nav-item active style="font-weight: 600" href="#"
-            >Metaverse</b-nav-item
-          >
-          <b-nav-item active style="font-weight: 600" href="#">News</b-nav-item>
-        </b-navbar-nav>
-      </b-navbar>
-    </div>
+    <b-navbar class="position-sticky" variant="white" type="light">
+      <b-navbar-brand href="#">
+        <img
+          src="/images/logo.svg"
+          class="d-inline-block align-top"
+          alt="logo "
+        />
+      </b-navbar-brand>
+      <b-navbar-nav>
+        <b-nav-item active style="font-weight: 600" href="#">Tech</b-nav-item>
+        <b-nav-item active style="font-weight: 600" href="#"
+          >Metaverse</b-nav-item
+        >
+        <b-nav-item active style="font-weight: 600" href="#">News</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
     <b-row
       style="height: calc(100vh - 86px)"
       class="px-5"
@@ -37,6 +34,7 @@
       <b-col>
         <b-card
           v-if="!play"
+          class="justify-content-center"
           no-body
           style="
             background-color: #ddd;
@@ -47,21 +45,29 @@
             height: 50vh;
           "
         >
-          <b-container class="h-100">
-            <b-row align-v="center" class="h-100">
-              <b-col class="text-center">
+          <b-container fluid>
+            <b-row align-h="center" align-v="center">
+              <b-col cols="12" md="6" sm="12" class="text-center">
                 <span style="font-weight: 800; font-size: 24px">MTRX Inc.</span>
                 <h1
                   style="
                     font-family: 'Open Sans';
                     font-style: normal;
                     font-weight: 800;
+                    font-size: 51px;
                     color: #5c5858;
                   "
                 >
-                  Welcome to the Mtrxverse.
+                  Enter the Mtrxverse.
                 </h1>
-                <b-button @click="onPlay">Play</b-button>
+              </b-col>
+              <b-col class="py-2" cols="12" md="6" sm="12">
+                <b-embed
+                  type="iframe"
+                  aspect="16by9"
+                  src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
+                  allowfullscreen
+                ></b-embed>
               </b-col>
             </b-row>
           </b-container>
@@ -82,6 +88,7 @@
 <script>
 export default {
   name: "IndexPage",
+  layout: "main",
   data() {
     return {
       play: false,
@@ -94,8 +101,3 @@ export default {
   },
 };
 </script>
-<style scoped>
-body {
-  background-image: url("/images/background.png");
-}
-</style>
