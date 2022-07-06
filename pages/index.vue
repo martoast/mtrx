f0961b9876638a6a522f885b1dbb6b6be48f888f
Alejandro Martos <alexmartos96@gmail.com>
2022-07-06 00:29:35 -0700

    no images

--- pages/index.vue
+++ pages/index.vue
@@ -121,21 +121,18 @@ export default {
           subtitle: "System Engineer",
           content:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-          image: "logo.png",
         },
         {
           title: "Milestone 2",
           subtitle: "Programmer",
           content:
             "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-          image: "logo.png",
         },
         {
           title: "Milestone 3",
           subtitle: "Internship / Volunteer",
           content:
             "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          image: "logo.png",
         },
       ],
     };
